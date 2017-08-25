9dec6f5831d7bbbdede0ae0eb088731a7e066bc2
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2017-08-25 18:44:06 +0100

    Initial ExHyperAuth repository

---
+++ lib/hyper_auth.ex
@@ -0,0 +1,18 @@
+defmodule HyperAuth do
+  @moduledoc """
+  Documentation for HyperAuth.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HyperAuth.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hyper_auth_test.exs
@@ -0,0 +1,8 @@
+defmodule HyperAuthTest do
+  use ExUnit.Case
+  doctest HyperAuth
+
+  test "greets the world" do
+    assert HyperAuth.hello() == :world
+  end
+end
