91fdb511114ab783ec62dbb80ee14a7e784a8093
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2017-08-25 19:35:20 +0100

    Add initial test

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule HyperAuth.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:plug, "~> 1.3.3 or ~> 1.4"}
     ]
   end
 end
--- test/hyper_auth_test.exs
+++ test/hyper_auth_test.exs
@@ -1,8 +1,84 @@
 defmodule HyperAuthTest do
-  use ExUnit.Case
-  doctest HyperAuth
+  use ExUnit.Case, async: true
+  use Plug.Test
 
-  test "greets the world" do
-    assert HyperAuth.hello() == :world
+  doctest HyperAuth
+  doctest HyperAuth.HTTP
+  doctest HyperAuth.Scheme.Basic
+  doctest HyperAuth.Scheme.Digest
+  doctest HyperAuth.Authenticator
+
+  test "HTTP public access 200" do
+    opts = HyperAuth.init([public: true])
+    conn = %{conn(:get, "/") | remote_ip: {192, 168, 1, 1}, scheme: :http}
+    |> put_status(200)
+    |> HyperAuth.call(opts)
+    assert conn.status == 200
+  end
+
+  test "HTTPS public access 200" do
+    opts = HyperAuth.init([public: true])
+    conn = %{conn(:get, "/") | remote_ip: {192, 168, 1, 1}, scheme: :https}
+    |> put_status(200)
+    |> HyperAuth.call(opts)
+    assert conn.status == 200
+  end
+
+  test "HTTPS unauthorized 401" do
+    opts = HyperAuth.init([])
+    conn = %{conn(:get, "/") | remote_ip: {192, 168, 1, 1}, scheme: :https}
+    |> put_status(200)
+    |> HyperAuth.call(opts)
+    assert conn.status == 401
+  end
+
+  test "HTTP forbidden 403" do
+    opts = HyperAuth.init([])
+    conn = %{conn(:get, "/") | remote_ip: {192, 168, 1, 1}, scheme: :http}
+    |> put_status(200)
+    |> HyperAuth.call(opts)
+    assert conn.status == 403
+  end
+
+  test "HTTP forbidden 403 authorization" do
+    opts = HyperAuth.init([])
+    conn = %{conn(:get, "/") | remote_ip: {192, 168, 1, 1}, scheme: :http}
+    |> put_req_header("authorization", "Basic dGVzdDp0ZXN0")
+    |> put_status(200)
+    |> HyperAuth.call(opts)
+    assert conn.status == 403
+  end
+
+  test "HTTP public access forbidden 403 authorization" do
+    opts = HyperAuth.init([public: true])
+    conn = %{conn(:get, "/") | remote_ip: {192, 168, 1, 1}, scheme: :http}
+    |> put_req_header("authorization", "Basic dGVzdDp0ZXN0")
+    |> put_status(200)
+    |> HyperAuth.call(opts)
+    assert conn.status == 403
+  end
+
+  test "HTTPS unauthorized 401 bad scheme" do
+    opts = HyperAuth.init([
+        authenticator: HyperAuth.Authenticator.Dummy,
+        authenticator_dummy_password: "test"
+      ])
+    conn = %{conn(:get, "/") | remote_ip: {192, 168, 1, 1}, scheme: :https}
+    |> put_req_header("authorization", "NotExists dGVzdDp0ZXN0")
+    |> put_status(200)
+    |> HyperAuth.call(opts)
+    assert conn.status == 401
+  end
+
+  test "HTTPS dummy authorized 200" do
+    opts = HyperAuth.init([
+        authenticator: HyperAuth.Authenticator.Dummy,
+        authenticator_dummy_password: "test"
+      ])
+    conn = %{conn(:get, "/") | remote_ip: {192, 168, 1, 1}, scheme: :https}
+    |> put_req_header("authorization", "Basic dGVzdDp0ZXN0")
+    |> put_status(200)
+    |> HyperAuth.call(opts)
+    assert conn.status == 200
   end
 end
