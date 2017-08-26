6df21c1e8c4f6ca9b8b79406e8ed2cc09dea5020
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2017-08-26 02:03:50 +0100

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -34,7 +34,8 @@ defmodule HyperAuth.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:plug, "~> 1.3.3 or ~> 1.4"}
+      {:plug, "~> 1.3.3 or ~> 1.4"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
