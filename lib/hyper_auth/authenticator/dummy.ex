85d4b0aacd44edf349f15559730b55237d5c98ba
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2017-08-26 12:29:10 +0100

    Add doctest for dummy authenticator

--- lib/hyper_auth/authenticator/dummy.ex
+++ lib/hyper_auth/authenticator/dummy.ex
@@ -4,6 +4,19 @@ defmodule HyperAuth.Authenticator.Dummy do
   Dummy authenticator.
   """
 
+  @doc """
+  Dummy authentication to use in test environment.
+
+  ## Examples
+
+    iex> HyperAuth.Authenticator.Dummy.authenticate(%{
+    ...> "User-Name" => "test",
+    ...> "User-Password" => "tset"
+    ...> }, [authenticator_dummy_password: "tset"])
+    %{
+      "uid" => "test"
+    }
+  """
   def authenticate(credentials, opts) do
     username = credentials["User-Name"]
     password = credentials["User-Password"]
