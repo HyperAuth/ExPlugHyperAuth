26c43c1af2c4748dfbb514869eb0edc68725efdc
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2017-08-28 00:03:21 +0100

    Update version to 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HyperAuth.Mixfile do
   def project do
     [
       app: :plug_hyper_auth,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.3",
       description: "Plug for HTTP authentication",
       start_permanent: Mix.env == :prod,
